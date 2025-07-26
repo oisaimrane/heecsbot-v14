fefebddbc88a75f44ef66013529f8c4ce7783558
oisaimrane <95139857+oisaimrane@users.noreply.github.com>
2025-07-26 08:35:00 +0100

    Update config.js

--- src/config.js
+++ src/config.js
@@ -4,12 +4,12 @@ module.exports = {
     prefix: "?", // Default prefix
     status: "dnd",
     eventListeners: 100,
-    developers: "627013557695021087",
+    developers: "793877159966015548",
     noPermsMessage: `You **do not** have the permission to do that!`,
 
     // Logging Channels //
-    slashCommandLoggingChannel: "1255137359205240853",
-    prefixCommandLoggingChannel: "1255137359205240853",
+    slashCommandLoggingChannel: "1309556699647967253",
+    prefixCommandLoggingChannel: "1309556699647967253",
 
     // EMBED COLOR //
     embedColor: "#E88D67",
@@ -19,6 +19,6 @@ module.exports = {
     embedWarning: "#fafc65",
     
     // CHANNEL IDS //
-    bugReportChannel: "1255155199329177672",
+    bugReportChannel: "1309556699647967253",
 
 }
